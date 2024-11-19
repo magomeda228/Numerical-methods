0478e58353a1f7020a28566855ece85f1ffe184a
Alexy Man <120121874+magomeda228@users.noreply.github.com>
2024-11-19 22:57:08 +0300

    Update lox228.py

--- lox228.py
+++ lox228.py
@@ -1,12 +1,1 @@
-import torch
-import random
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-def func(x):
-    return 2*np.exp(-np.abs(x+1)/2)+2*np.exp(-np.abs(x-1)/2)
-x_val=np.linspace(-10,10,100)
-y_val=func(x_val)
-sns.set(style='whitegrid')
-sns.lineplot(x=x_val,y=y_val)
-plt.show()
+
